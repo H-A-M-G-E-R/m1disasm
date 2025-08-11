28abcc3172aa9d343b42d6c098def1373f654f89
H-A-M-G-E-R <hamgerham@proton.me>
2025-08-11 01:04:23 -0500

    bullet hit and elevator stuff

--- SRC/prg3_tourian.asm
+++ SRC/prg3_tourian.asm
@@ -1402,12 +1402,12 @@ MotherBrain_SpawnDoor:
     sta ObjY,x
     ; init door animation (because of the 1/2 chance that it plays only for 1 frame,
     ; the first frame has to be $F7)
-    lda #ObjAnim_55 - ObjectAnimIndexTbl.b
+    lda #ObjAnim_DoorOpen_Reset - ObjectAnimIndexTbl.b
     sta ObjAnimResetIndex,x
     sta ObjAnimIndex,x
     lda #$00
     sta ObjAnimDelay,x
-    lda #$F7
+    lda #_id_ObjFrame_DoorOpened.b
     sta ObjAnimFrame,x
     ; create door tiles
     lda #$10
