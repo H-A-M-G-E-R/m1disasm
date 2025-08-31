e8dc5388347aa906fe6b3b03670bba12c8615f1f
H-A-M-G-E-R <hamgerham@proton.me>
2025-08-30 19:50:31 -0500

    Fix pickups

--- SRC/prg7_engine.asm
+++ SRC/prg7_engine.asm
@@ -8860,7 +8860,7 @@ UpdateEnemy_Pickup: ;($F483)
     
     ; if anim frame is #$80, it is a missile pickup
     ldy EnsExtra.0.animFrame,x
-    cpy #_id_EnFrame_MissilePickup.b
+    cpy AreaMissilePickupAnimFrame
     beq @pickupMissile
     
     ; health pickup
@@ -8879,7 +8879,7 @@ UpdateEnemy_Pickup: ;($F483)
         dex
         pla
         ; branch if not small health pickup
-        cmp #_id_EnFrame_SmallEnergyPickup.b
+        cmp AreaSmallEnergyPickupAnimFrame
         bne @endIf_B
             ; small health pickup
             ;Increase Health by 5.
